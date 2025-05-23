a1c6cd0adb0a7ba114c56e6e107406454dbea769
pamela <plotriet@retrorabbit.co.za>
2025-05-23 18:13:49 +0200

    init components created

---
+++ nooklist/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAl8bdU1wf5Xq4oD35KvnN6MiqzG8HHjfo',
+    authDomain: 'nooklist-2a579.firebaseapp.com',
+    projectId: 'nooklist-2a579',
+    storageBucket: 'nooklist-2a579.firebasestorage.app',
+    messagingSenderId: '180326202211',
+    appId: '1:180326202211:web:0e6b0111751bfbbe3ef156',
+    measurementId: 'G-H8ZXXJ1VCL',
+  },
+};
